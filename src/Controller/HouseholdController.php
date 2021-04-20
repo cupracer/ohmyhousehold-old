84850ed73c72399fba8ce36c9c730b562307b48d
Thomas Schulte <thomas@cupracer.de>
2021-04-20 14:58:11 +0200

    add dummy api check

--- src/Controller/HouseholdController.php
+++ src/Controller/HouseholdController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +15,15 @@ class HouseholdController extends AbstractController
     {
         return $this->render('homepage.html.twig', [
             'controller_name' => 'HouseholdController',
+        ]);
+    }
+
+    #[Route('/api/check', name: 'api_check')]
+    #[IsGranted("ROLE_USER")]
+    public function apiCheck(): Response
+    {
+        return new JsonResponse([
+            'success' => true
         ]);
     }
 }
