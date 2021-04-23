8128656f31cdf6a1dd142eb2e62f0be310d7b8ca
Thomas Schulte <thomas@cupracer.de>
2021-04-23 16:10:27 +0200

    reply with JsonResponse on API requests

--- src/Security/AccessDeniedHandler.php
+++ src/Security/AccessDeniedHandler.php
@@ -2,6 +2,7 @@
 
 namespace App\Security;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -22,6 +23,12 @@ class AccessDeniedHandler implements AccessDeniedHandlerInterface
 
     public function handle(Request $request, AccessDeniedException $accessDeniedException)
     {
+        if(str_starts_with($request->attributes->get('_route'), 'api_')) {
+            return new JsonResponse([
+                'success' => false,
+                "message" => "Permission denied."
+            ]);
+        }
 
         // add a custom flash message and redirect to the login page
         $this->session->getFlashBag()->add('info', 'You don\'t have the permission to access this page.');
