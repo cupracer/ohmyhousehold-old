7876e04570a64efe609bfab6d5e06884b8372aeb
Thomas Schulte <thomas@cupracer.de>
2021-04-23 15:05:56 +0200

    clean up unused import

--- src/Controller/HouseholdController.php
+++ src/Controller/HouseholdController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
