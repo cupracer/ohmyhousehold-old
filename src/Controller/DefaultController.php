afd69569e9d545391d9014b66212bd748541ba05
Thomas Schulte <thomas@cupracer.de>
2021-05-14 09:20:09 +0200

    clean up project name

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Service\ReferrerService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
-class HouseholdController extends AbstractController
+class DefaultController extends AbstractController
 {
     #[Route('/', name: 'start')]
-    public function indexNoLocale(SessionInterface $session): Response
+    public function homepageNoLocale(SessionInterface $session): Response
     {
         if($session->get('_locale')) {
             return $this->redirectToRoute('homepage', ['_locale' => $session->get('_locale')]);
@@ -23,11 +26,32 @@ class HouseholdController extends AbstractController
     }
 
     #[Route('/{_locale<%app.supported_locales%>}/', name: 'homepage')]
-    public function index(): Response
+    public function homepage(): Response
     {
-        return $this->render('homepage.html.twig', [
-            'controller_name' => 'HouseholdController',
-        ]);
+        return $this->render('homepage.html.twig');
+    }
+
+    #[Route('/user/locale/{_locale<%app.supported_locales%>}/', name: 'app_user_locale')]
+    public function index(string $_locale, Request $request, ReferrerService $referrerService): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if($user) {
+            $user->getUserProfile()->setLocale($request->getSession()->get('_locale'));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+        }
+
+        $referrerUrl = $referrerService->getReferrerUrl($request);
+
+        if($referrerUrl) {
+            //TODO: Do we need to use URL query parameters as well?
+            return $this->redirectToRoute($referrerUrl, ['_locale' => $_locale]);
+        }
+
+        return $this->redirectToRoute('homepage', ['_locale' => $_locale]);
     }
 
     #[Route('/api/ping', name: 'api_ping')]
