bd36aeedc18eb87cf8225edf58c187d5fa8d4929
Thomas Schulte <thomas@cupracer.de>
2021-08-25 09:28:02 +0200

    fix default household detection

--- src/EventSubscriber/HouseholdSubscriber.php
+++ src/EventSubscriber/HouseholdSubscriber.php
@@ -21,18 +21,28 @@ class HouseholdSubscriber implements EventSubscriberInterface
 
     public function onLoginSuccess(LoginSuccessEvent $event)
     {
+        //fallback
+        $householdId = 0;
+
         /** @var User $user */
         $user = $event->getUser();
 
         // The user's session should always contain a "current_household" value.
-        // To ensure that it is set upon loggin in, we search for all repositories
+
+        // To ensure that it is set upon logging in, we search for all repositories
         // where the user has admin rights and pick the first one.
-        // TODO: Make current_household configurable via user preferences.
         $households = $this->householdRepository->findByAdmin($user);
 
         if(is_array($households) && sizeof($households) > 0) {
-            $this->session->set('current_household', $households[0]->getId());
+            $householdId = $households[0]->getId();
+        }else {
+            // if no administrated household is found, try to find one with a normal membership
+            if($user->getHouseholdUsers()) {
+                $householdId = $user->getHouseholdUsers()->toArray()[0]->getHousehold()->getId();
+            }
         }
+
+        $this->session->set('current_household', $householdId);
     }
 
     public static function getSubscribedEvents()
