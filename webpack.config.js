49fe3554126eef80dbe4ec2259bca3241bbb48c6
Thomas Schulte <thomas@cupracer.de>
2021-08-31 22:46:32 +0200

    add application logo

--- webpack.config.js
+++ webpack.config.js
@@ -138,6 +138,11 @@ Encore
             moment: "moment"
         })
     )
+
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[ext]',
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
